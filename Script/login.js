47ded6e17629a6ead0fe072818fd678308c6b63a
unknown <ayondevs.94b@gmail.com>
2026-02-20 18:02:19 +0530

    user login js logic sucessfully added

---
+++ Script/login.js
@@ -0,0 +1,15 @@
+document.getElementById("login-btn").addEventListener("click", function () {
+  const inputNum = document.getElementById("input-num");
+  const contactNum = inputNum.value;
+
+  const inputPin = document.getElementById("input-pin");
+  const pinValue = inputPin.value;
+  console.log(contactNum, "the pin is", pinValue);
+
+  if (contactNum == "7980441957" && pinValue == "1234") {
+    alert("login success");
+    window.location.assign("/home.html");
+  } else {
+    alert("wrong credentials");
+  }
+});
